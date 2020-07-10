cb54bfb43efb1247a503031fcae2bf8647026ee7
Giampierre Huaman Berru <2017210499@untels.edu.pe>
2020-07-09 21:24:25 -0500

    Pregunta03-Listo

--- Preg03.py
+++ Preg03.py
@@ -1,1 +1,10 @@
 #Autor: Giampierre Huaman Berru
+
+def contador():
+    
+    texto = input("Ingrese texto: ")
+    espacio = " "
+    print( len([letra for letra in texto if letra not in espacio]))
+
+
+contador()
