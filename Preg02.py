da7f25caafe9a59b70756e01f34f00b6c78b8ab7
Pierre <pieerre95x3@gmail.com>
2020-07-09 21:14:36 -0500

    Pregunta02-Listo

--- Preg02.py
+++ Preg02.py
@@ -1,1 +1,14 @@
 #AUTOR: BERNAOLA MORENO PIERRE
+
+def comparar():
+    
+    variable = input("Ingrese correo de la untels: ")
+    
+    untels = "untels.edu.pe"
+    
+    if variable.split("@")[1] == untels:
+        print("ES CORREO DE LA UNIVERSIDAD")
+    else:
+        print("NO ES CORREO DE LA UNIVERSIDAD")
+        
+comparar()
