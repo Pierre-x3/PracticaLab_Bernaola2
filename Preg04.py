9563c6d111d9e4f64e0cd640610e8e47d433348d
Pierre <pieerre95x3@gmail.com>
2020-07-09 21:32:29 -0500

    Pregunta04-Listo y Desarrolladores registrados por Bernaola Moreno Pierre

--- Preg04.py
+++ Preg04.py
@@ -1,1 +1,13 @@
 #AUTOR: BERNAOLA MORENO PIERRE OSCAR
+
+def invertir():
+    num=input("Ingrese el numero a invertir: ")
+
+    invertido=reversed(num)
+    [char for char in reversed(num)]
+
+    resultado="".join(invertido)
+    print("El numero ivnertido es: ") 
+    print(resultado)
+
+invertir()
